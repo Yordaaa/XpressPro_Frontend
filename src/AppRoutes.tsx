5ca1c6f175a28002c0392b93b10a0e8633dd6014
YerosenHunduma <yerosenhunduma@gmail.com>
2024-08-12 11:13:37 +0300

    remove unwanted import form AppRoutes component

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,29 +1,26 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import Cars from "./Pages/Cars";
-import Booking from "./Pages/Booking";
-import ContactUs from "./Pages/ContactUs";
-import AboutUs from "./Pages/AboutUs";
-import Login from "./Pages/AuthPages/Login";
-import Signup from "./Pages/AuthPages/Signup";
-import AdditionalService from "./Components/AdditionalService";
-import Car from "./Components/Car";
-import Review from "./Pages/Review";
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Cars from './Pages/Cars';
+import Booking from './Pages/Booking';
+import ContactUs from './Pages/ContactUs';
+import AboutUs from './Pages/AboutUs';
+import Login from './Pages/AuthPages/Login';
+import Signup from './Pages/AuthPages/Signup';
+import Review from './Pages/Review';
 
 function AppRoutes() {
-  return <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="cars" element={<Cars />} />
-    <Route path="booking" element={<Booking />} />
-    <Route path="contactus" element={<ContactUs />} />
-    <Route path="aboutus" element={<AboutUs />} />
-    <Route path="login" element={<Login />} />
-    <Route path="signup" element={<Signup />} />
-    <Route path="additional" element={<AdditionalService />} />
-    <Route path="car" element={<Car />} />
-    <Route path="review" element={<Review />} />
-  </Routes>;
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="cars" element={<Cars />} />
+            <Route path="booking" element={<Booking />} />
+            <Route path="contactus" element={<ContactUs />} />
+            <Route path="aboutus" element={<AboutUs />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="review" element={<Review />} />
+        </Routes>
+    );
 }
 
 export default AppRoutes;
